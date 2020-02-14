99757fa748cc1f2eb64329d15681f238f75b502f
kengne ruphine <ruphinekengne@gmail.com>
2020-02-14 23:39:17 +0100

    setting up formating tools and installing yarn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const logger = require('morgan') // It is a http request logger which print in the terminal all the request that the server will receive
+const v1 = require('./v1')
 
-module.exports = app;
+/**
+* Ensure JSON acceptance
+ */
+app.use((req, res, next) => {
+  let err
+
+  if (!req.accepts('json')) {
+    err = new Error('Not acceptable')
+    err.status = 406
+  }
+  return next(err)
+})
+
+/**
+ *  Middlewares
+ */
+app.use(logger('dev'))
+
+/**
+ * Routes
+ */
+app.use('/v1', v1)
+
+/**
+ * ErrorHandler
+*/
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+    .json({
+      message: err.message,
+      stack: err.stack
+    })
+})
+
+module.exports = app
